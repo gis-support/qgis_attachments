6f5658d6021cc3da98b3cf2860519ff133448965
Hubert Zakoscielny <hz@gis-support.pl>
2020-05-20 01:30:08 +0200

    QGS-106 QGS-107 QGS-105 Zapisywanie załączników podczas dodawania, poprawienie niestabilności wtyczki i część refaktoringu kodu backendu geopackage

--- backends/layers/main.py
+++ backends/layers/main.py
@@ -27,9 +27,11 @@ class LayersBackend(BackendAbstract):
         ], parent=parent)
         self.model = LayersModel(columns=['Opcje', 'Pliki'], separator=self.SEPARATOR)
         self.connection = None
+        '''
         self.featureActionDlg = None
         self.newFeatureAdded = None
         self.newFeatureIds = []
+        '''
         self.geopackage_path = self.parent.layer().dataProvider().dataSourceUri().split('|')[0]
 
     #Ustawienia
@@ -58,6 +60,7 @@ class LayersBackend(BackendAbstract):
             )
             return
         self.connect()
+        '''
         #Czyszczenie wpisów w bazie, jeśli tworzenie nowego obiektu nie zostanie zatwierdzone
         if not self.featureActionDlg:
             self.featureActionDlg = self.isFeatureActionDlgOpened()
@@ -65,15 +68,32 @@ class LayersBackend(BackendAbstract):
                 self.newFeatureAdded = True
                 self.featureActionDlg.rejected.connect(self.featureActionDlgRejected)
                 self.featureActionDlg.accepted.connect(self.featureActionDlgAccepted)
+        '''
         self.parent.widget.setFocus()
         files, _ = QFileDialog.getOpenFileNames(self.parent.widget, 'Wybierz załączniki')
         files_indexes = self.saveAttachments(files)
         result = self.model.insertRows(files_indexes, max_length=self.parent.field().length())
+        
+        field_id = self.parent.fieldIdx()
+        try:
+            current_value = self.parent.context().formFeature().attribute(field_id)
+            feat_id = self.parent.context().formFeature().id()
+        except KeyError:
+            current_value = self.parent.formFeature().attribute(field_id)
+            feat_id = self.parent.formFeature().id()
+        value = None
+        if current_value:
+            value = current_value + ';' + ';'.join([v[0] for v in files_indexes])
+        else:
+            value = ';'.join([v[0] for v in files_indexes])
+        self.parent.layer().dataProvider().changeAttributeValues({feat_id: {field_id: value}})
+        self.parent.layer().reload()
         return result
 
     def deleteAttachment(self):
         """Usuwa załącznik"""
-        self.checkConnection()
+        if not self.connection:
+            self.connect()
         selected = self.parent.widget.tblAttachments.selectedIndexes()
         if len(selected) < 1:
             self.parent.bar.pushCritical(
@@ -82,17 +102,33 @@ class LayersBackend(BackendAbstract):
             )
             return
         index = selected[0]
+        value_to_delete = self.model.data(index, field='id')
         sql = """DELETE FROM qgis_attachments where id = {}"""
         cursor = self.connection.cursor()
-        cursor.execute(sql.format(self.model.data(index, field='id')))
+        cursor.execute(sql.format(value_to_delete))
         self.connection.commit()
         self.model.removeRow(index.row())
+
+        field_id = self.parent.fieldIdx()
+        try:
+            current_value = self.parent.context().formFeature().attribute(field_id)
+            feat_id = self.parent.context().formFeature().id()
+        except KeyError:
+            current_value = self.parent.formFeature().attribute(field_id)
+            feat_id = self.parent.formFeature().id()
+        if len(current_value) > 1:
+            value = ';'.join([v for v in current_value.split(';') if v != value_to_delete])
+        elif len(current_value) == 1:
+            value = (current_value[0])
+        else:
+            value = NULL
+        self.parent.layer().dataProvider().changeAttributeValues({feat_id: {field_id: value}})
         cursor.close()
-        self.connection.close()
 
     def saveAttachments(self, files_list):
         """Zapisuje załączniki i zwraca listę id"""
-        self.checkConnection()
+        if not self.connection:
+            self.connect()
         sql = """INSERT INTO qgis_attachments (name, data) VALUES (?, ?)"""
         cursor = self.connection.cursor()
         ids = []
@@ -102,10 +138,12 @@ class LayersBackend(BackendAbstract):
                 blob = f.read()
                 cursor.execute(sql, (name, sqlite3.Binary(blob)))
                 attachment_id = str(cursor.lastrowid)
+                '''
                 if self.newFeatureAdded:
                     self.newFeatureIds.append(attachment_id)
+                '''
                 ids.append([attachment_id, name])
-                self.connection.commit()
+        self.connection.commit()
         cursor.close()
         return ids
 
@@ -123,7 +161,8 @@ class LayersBackend(BackendAbstract):
                 #Anulowanie zapisywania
                 return
 
-        self.checkConnection()
+        if not self.connection:
+            self.connect()
         save_dir = ''
         sql = """SELECT name, data FROM qgis_attachments WHERE id = {}"""
         cursor = self.connection.cursor()
@@ -153,35 +192,32 @@ class LayersBackend(BackendAbstract):
             )"""
         self.connection.execute(sql)
 
+    '''
     def isFeatureActionDlgOpened(self):
         """Sprawdza czy otwarte jest okno tworzenia nowego obiektu"""
         for obj in QApplication.instance().allWidgets():
             if self.parent.layer().name() in obj.objectName() and isinstance(obj, QDialog): 
                 return obj
         return False
+    '''
 
     def getFilenames(self, values):
         """Dodaje informacje o nazwach plików do listy id"""
-        self.checkConnection()
         sql = """SELECT name FROM qgis_attachments WHERE id = {}"""
         values_filenames = []
-        cursor = self.connection.cursor()
-        for value in values:
-            try:
-                query_output = cursor.execute(sql.format(value)).fetchone()
-            except sqlite3.OperationalError:
-                query_output = None
-            if query_output is None:
-                return []
-            elif len(query_output) > 0:
-                values_filenames.append([value, query_output[0]])
-        return values_filenames
-
-    def checkConnection(self):
-        """Sprawdza czy istnieje połączenie z bazą, tworzy je jeśli nie istnieje"""
         if not self.connection:
             self.connect()
+        cursor = self.connection.cursor()
+        for value in values:
+            query_output = cursor.execute(sql.format(value)).fetchone()
+            if query_output is None:
+                query_output = cursor.execute(sql.format(value)).fetchone()
+            elif len(query_output) > 0:
+                values_filenames.append([value, query_output[0]])
+        cursor.close()
+        return values_filenames
 
+'''
     def featureActionDlgAccepted(self):
         self.newFeatureAdded = None
         self.newFeatureIds = []
@@ -194,3 +230,4 @@ class LayersBackend(BackendAbstract):
         self.connection.commit()
         cursor.close()
         self.newFeatureIds = []
+'''
